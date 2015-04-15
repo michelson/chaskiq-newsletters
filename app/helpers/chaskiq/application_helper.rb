86b697a10d52280fc20e2f30bb31acaf384fd471
miguel michelson <miguelmichelson@gmail.com>
2015-04-14 23:44:20 -0300

    kaminari views

--- app/helpers/chaskiq/application_helper.rb
+++ app/helpers/chaskiq/application_helper.rb
@@ -1,6 +1,11 @@
 module Chaskiq
   module ApplicationHelper
 
+    def paginate objects, options = {}
+      options.reverse_merge!( theme: 'twitter-bootstrap-3' )
+      super( objects, options )
+    end
+
   end
 
 end
