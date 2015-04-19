6920b45c38a012093cb63c9db958df0ffba63c52
miguel michelson <miguelmichelson@gmail.com>
2015-04-19 20:44:45 -0300

    styles and charts for dashboard & campaign show

--- app/controllers/chaskiq/dashboard_controller.rb
+++ app/controllers/chaskiq/dashboard_controller.rb
@@ -8,6 +8,7 @@ module Chaskiq
       @campaigns_count = Chaskiq::Campaign.count
       @sends_count = Chaskiq::Metric.deliveries.size
       @daily_metrics = Chaskiq::Metric.group_by_day(:created_at, range: 2.weeks.ago.midnight..Time.now).count
+      @pie_metrics = Chaskiq::Metric.group(:action)
     end
   end
 end
